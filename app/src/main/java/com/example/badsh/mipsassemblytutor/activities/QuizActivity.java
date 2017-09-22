803f3042aef043be7eddcd6e26f1793c393bac89
Shabaz Badshah <badshah.shabaz@gmail.com>
2017-09-22 08:56:59 -0400

    finished a very basic version of the quizComplete screen

--- app/src/main/java/com/example/badsh/mipsassemblytutor/activities/QuizActivity.java
+++ app/src/main/java/com/example/badsh/mipsassemblytutor/activities/QuizActivity.java
@@ -1,8 +1,10 @@
 package com.example.badsh.mipsassemblytutor.activities;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.badsh.mipsassemblytutor.R;
 
+import static android.R.string.no;
 import static com.example.badsh.mipsassemblytutor.R.id.quitQuiz;
 import static com.example.badsh.mipsassemblytutor.R.id.requestHint;
 
@@ -31,12 +34,14 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private Button confirmAnswerBtn;
     private int amountUserHintsLeft = 2;
     private int currentUserQuestionNum = 1;
-    private int totalAmountQuestions = 24;
+    private int totalAmountQuestions = 1;
+    private long timeWhenStopped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_activity);
+
         initToolbarsAndQuizBG();
         initHintsAndQuestionNumCounts();
         initQuizTimer();
@@ -77,18 +82,21 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         confirmAnswerBtn.setOnClickListener(this);
     }
 
+    public void onResume() {
+        super.onResume();
+        quizElapsedTimerView.start();
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.quitQuiz:
-                if (currentUserQuestionNum > totalAmountQuestions) {
-                    finish();
-                } else {
-                    onBackPressed();
-                }
+                onBackPressed();
                 break;
             case R.id.requestHint:
+                pauseQuizTimer();
                 if (amountUserHintsLeft <= 0) {
+                    resumeQuizTimer();
                     Toast.makeText(this, "You do not have any hints left", Toast.LENGTH_SHORT).show();
                 } else {
                     promptUserForConfirmation("Do you want to use a hint?", "Hint", R.id.requestHint);
@@ -96,7 +104,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.confirmAnswerBtn:
                 if (currentUserQuestionNum >= totalAmountQuestions) {
-                    finish();
+                    finishGame();
                 } else {
                     incrementQuestionNumber();
                 }
@@ -105,7 +113,19 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onBackPressed() {
+        pauseQuizTimer();
         promptUserForConfirmation("Are you sure you want to quit the quiz?", "Quit", R.id.quitQuiz);
+    }
+
+    private void pauseQuizTimer(){
+        quizElapsedTimerView.stop();
+        timeWhenStopped = quizElapsedTimerView.getBase() - SystemClock.elapsedRealtime();
+    }
+
+    private void resumeQuizTimer() {
+        quizElapsedTimerView.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+        timeWhenStopped = 0;
+        quizElapsedTimerView.start();
     }
 
     private void promptUserForConfirmation(String messageToDisplayOnPrompt, String titleOfPrompt, final int itemClicked) {
@@ -122,14 +142,19 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
                                 break;
                             case R.id.requestHint:
                                 decrementHintCount();
+                                resumeQuizTimer();
                                 break;
                             case R.id.quitQuiz:
-                                quizElapsedTimerView.stop();
                                 finish(); // Finishes the current activity and goes to the previous one
                                 break;
                         }
                     }})
-                .setNegativeButton(android.R.string.no, null)
+                .setNegativeButton(no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        resumeQuizTimer();
+                    }
+                })
                 .show();
     }
 
@@ -147,5 +172,18 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
             currentQuestionNumView.setText(questionCountDisplayFormat);
         }
     }
+
+    private void finishGame() {
+        Intent quizCompleteIntent = new Intent(getApplicationContext(), QuizCompleteActivity.class);
+        String userScoreFormat = String.format("%d/%d", 3, totalAmountQuestions);
+        quizCompleteIntent.putExtra("userScore", userScoreFormat);
+
+        long totalElapsedTimeInSec = SystemClock.elapsedRealtime() - quizElapsedTimerView.getBase();
+        quizCompleteIntent.putExtra("userTime", String.valueOf(totalElapsedTimeInSec/1000));
+
+        getApplicationContext().startActivity(quizCompleteIntent);
+        finish();
+    }
+
 }
 
