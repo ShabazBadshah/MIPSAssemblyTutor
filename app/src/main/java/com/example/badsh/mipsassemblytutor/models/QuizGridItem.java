a493f14d388223001d78c067dd643d13e01940fa
Shabaz Badshah <badshah.shabaz@gmail.com>
2017-09-22 09:44:46 -0400

    reorganizied QuizGridItem and RVAdapter, GridItem currently launches MainActivity

--- app/src/main/java/com/example/badsh/mipsassemblytutor/models/QuizGridItem.java
+++ app/src/main/java/com/example/badsh/mipsassemblytutor/models/QuizGridItem.java
@@ -3,7 +3,8 @@ package com.example.badsh.mipsassemblytutor.models;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.widget.Toast;
+
+import com.example.badsh.mipsassemblytutor.MainActivity;
 
 /**
  * Created by Shabaz Badshah on 9/16/2017.
@@ -12,47 +13,51 @@ import android.widget.Toast;
 public class QuizGridItem {
     private String mNameOfQuiz;
     private int mQuizPrimaryColor;
+    private int mQuizDarkPrimaryColor;
     private Class mQuizActivityToStart;
-
     private Context mParentActivityContext;
 
     public QuizGridItem(Class quizActivityToStart) {
-       this.mQuizActivityToStart = quizActivityToStart;
+        this.mQuizActivityToStart = quizActivityToStart;
     }
 
-    private void startQuizActivity() {
-        Intent intentToStartQuiz = new Intent(mParentActivityContext, mQuizActivityToStart);
-        intentToStartQuiz.putExtra("mNameOfQuiz", mNameOfQuiz);
-
+    public void startQuizActivity() {
+        Intent intentToStartQuiz = new Intent(mParentActivityContext, MainActivity.class);
         // https://stackoverflow.com/questions/6539879/how-to-convert-a-color-integer-to-a-hex-string-in-android
         String quizPrimaryColor = String.format("#%06X", (0xFFFFFF & mQuizPrimaryColor));
+        String quizDarkPrimaryColor = String.format("#%06X", (0xFFFFFF & mQuizPrimaryColor));
+
         intentToStartQuiz.putExtra("mQuizPrimaryColor", quizPrimaryColor);
+        intentToStartQuiz.putExtra("mQuizDarkPrimaryColor", quizDarkPrimaryColor);
         mParentActivityContext.startActivity(intentToStartQuiz);
     }
 
     public void setContext(Context parentActivityContext) {
-       this.mParentActivityContext = parentActivityContext;
+        this.mParentActivityContext = parentActivityContext;
     }
 
     public void setQuizPrimaryColor(String colorToSet) {
-       this.mQuizPrimaryColor = Color.parseColor(colorToSet);
+        this.mQuizPrimaryColor = Color.parseColor(colorToSet);
+    }
+
+    public void setQuizDarkPrimaryColor(String darkColorToSet) {
+        this.mQuizDarkPrimaryColor = Color.parseColor(darkColorToSet);
     }
 
     public void setNameOfQuiz(String nameToSet) {
-       this.mNameOfQuiz = nameToSet;
+        this.mNameOfQuiz = nameToSet;
     }
 
     public int getPrimaryColor() {
-       return this.mQuizPrimaryColor;
+        return this.mQuizPrimaryColor;
     }
 
-    public void onClick(Context parentContext) {
-        Toast.makeText(parentContext, this.toString(), Toast.LENGTH_SHORT).show();
-        startQuizActivity();
+    public int getDarkPrimaryColor() {
+        return this.mQuizDarkPrimaryColor;
     }
 
     public String getNameOfQuiz() {
-       return this.mNameOfQuiz;
+        return this.mNameOfQuiz;
     }
 
     public String toString() {
